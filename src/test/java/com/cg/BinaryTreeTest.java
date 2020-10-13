09867a478e4de888db0a7a2afccf257f931dfe87
Arijit20 <ARIJIT DEY@LAPTOP-F4J8SDAU>
2020-10-13 22:31:02 +0530

    [Arijit]Added : Created BST And Added Elements

--- src/test/java/com/cg/BinaryTreeTest.java
+++ src/test/java/com/cg/BinaryTreeTest.java
@@ -4,8 +4,17 @@ import org.junit.Test;
 
 import com.cg.BinaryTree;
 
+import junit.framework.Assert;
+
 import static org.junit.Assert.*;
 
 public class BinaryTreeTest {
-  
+  @Test
+  public void numbersAddedToTreeReturnSize() {
+	  BinaryTree<Integer> binaryTree = new BinaryTree<>();
+	  binaryTree.add(56);
+	  binaryTree.add(30);
+	  binaryTree.add(70);
+	  Assert.assertEquals(3, binaryTree.getSize());
+  }
 }
